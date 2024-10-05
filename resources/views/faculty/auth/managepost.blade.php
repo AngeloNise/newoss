ec54b3af6825010d307136da8a52974d837687e2
AngeloNise <mark.angelo.aboy@gmail.com>
2024-10-05 12:28:02 +0800

    newcommit

---
+++ resources/views/faculty/auth/managepost.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.adminlayout')
+
+@section('content')
+@endsection
---
+++ storage/framework/views/3a05fd14b880e8ca719fe208632a564a.php
@@ -0,0 +1,16 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="content-container">
+    <h1>Evaluation Form</h1>
+    <div class="activity-question">
+        <p>What type of Activity?</p>
+    </div>
+    <div class="activity-buttons">
+        <a href="<?php echo e(url('/Fund-Raising-Forms')); ?>" class="button">Fund Raising Activity</a>
+        <a href="<?php echo e(url('/In-Campus-Forms')); ?>" class="button">In-Campus Activity</a>
+        <a href="<?php echo e(url('/Off-Campus-Forms')); ?>" class="button">Off-Campus Activity</a>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.adminlayout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\College\oss\resources\views//faculty/auth/preevalstatus.blade.php ENDPATH**/ ?>
